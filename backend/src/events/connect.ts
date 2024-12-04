e86517303f449ee6ced44ccca6c34fc03d698dd0
eniyenzima002 <eniyenzima@gmail.com>
2024-12-03 23:48:40 -0600

    Few file changes

--- backend/src/events/connect.ts
+++ backend/src/events/connect.ts
@@ -15,38 +15,11 @@ export class ConnectUser {
     async onConnection(@ConnectedSocket() socket: Socket,
         @SocketIO() io: Server
     ) {
-        console.log("New Socket player connected: ", socket.id)
-        
-        // __INIT-PLAYER-TOKEN__
-        // const token = socket.handshake.auth.player as any;
-        // this.player = await getActivePlayer(token)
-        // console.log("Socket connected credentials: ", this.player.id);
-
-
-        // __ONLINE__
-        // socket.join(this.player.id?.toString());
-        // this.onlinePlayer.add(this.player?.id.toString());
-        // io.emit("get_online_player", Array.from(this.onlinePlayer));
-
-        // __AUTH__
-        // socket.on("auth_player", async (authId) => {
-        //     console.log("auth id", authId)
-        //     const authPlayer = await prisma.user.findUnique({ where: { id: authId } });
-            
-        //     const authPayload = {
-        //         id: authPlayer?.id,
-        //         username: authPlayer?.username,
-        //         online: this.onlinePlayer.has(authId)
-        //     }
-
-        //     io.emit("authenticated_player", authPayload);
-        // })
-        
+        console.log("New Socket player connected: ", socket.id);
     }
 
     @OnDisconnect()
     onDisconnect(@ConnectedSocket() socket: Socket) {
-        // this.onlinePlayer.delete(this.player?.id);
         console.log("Player disconnected: ", socket.id); 
     }
 }
