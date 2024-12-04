0de915bd0a2deb721f939f3bb27103a7280c8cfd
eniyenzima002 <eniyenzima@gmail.com>
2024-12-04 05:19:49 -0600

    C

--- backend/dist/src/app/definition.js
+++ backend/dist/src/app/definition.js
@@ -62,7 +62,7 @@ class ExpressApplication {
         if (process.env.NODE_ENV !== "development") {
             this.app.use(express_1.default.static(path_1.default.join(__dirname, "/frontend/dist")));
             this.app.get("*", (req, res) => {
-                res.sendFile(path_1.default.join(process.cwd(), "frontend", "dist", "index.html"));
+                res.sendFile(path_1.default.join(__dirname, "frontend", "dist", "index.html"));
             });
         }
     }
